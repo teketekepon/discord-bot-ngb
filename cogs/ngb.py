ad80ff96c8e19c6a22a42710e450604eae4512c0
Takahiro Furukawa <teketeke.0408@gmail.com>
2020-09-02 17:53:38 +0900

    make it better

--- cogs/ngb.py
+++ cogs/ngb.py
@@ -68,9 +68,9 @@ class Ngb(commands.Cog):
         dm = member.dm_channel
         if dm is None:
             dm = await member.create_dm()
-        await dm.send(f'{member.mention} さん。ナイトガーデングループへようこそ！\n'\
-                    　'温泉郷チャンネルで「/bot」と発言すると、あなたの所属するクラン'\
-                    　'専用のチャットが見れるようになります。ご活用ください！')
+        await dm.send(f'{member.mention} さん。ナイトガーデングループへようこそ！\n'
+                      '温泉郷チャンネルで「/bot」と発言すると、あなたの所属するクラン'
+                      '専用のチャットが見れるようになります。ご活用ください！')
 
 # Bot本体側からコグを読み込む際に呼び出される関数。
 def setup(bot):
