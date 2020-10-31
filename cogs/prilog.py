ff42f81e443929e6e41b5fffa19071d2988e62d8
Teketeke <teketeke.0408@gmail.com>
2020-10-31 21:01:55 +0900

    make it better

--- cogs/prilog.py
+++ cogs/prilog.py
@@ -7,7 +7,7 @@ PRILOG_TOKEN = os.environ["PRILOG_TOKEN"]
 
 
 class PriLog(commands.Cog):
-    """Prilog APIを利用するためのコグ"""
+    """Prilog APIを利用します。"""
 
     def __init__(self, bot):
         self.bot = bot
@@ -17,7 +17,6 @@ class PriLog(commands.Cog):
         async with aiohttp.CliantSession() as session:
             async with session.get('https://prilog.jp/rest/analyze'
                                    f'?Url={url}&Token={PRILOG_TOKEN}') as r:
-                self.logger.info(f'Try to get:{url}... status={r.status}')
                 try:
                     return await r.json()
                 except Exception:
@@ -25,9 +24,18 @@ class PriLog(commands.Cog):
 
     @commands.command()
     async def log(self, ctx, url: str):
-        resp = self.get_response(url)
+        """
+        引数にクランバトル動画を渡すとPriLogを利用できます。
+        コマンドとURLの間に半角スペースを空けてください。
+        例: /log https://www.youtube.com/watch?v=mvLSw5vCpGU
+        """
+        resp = await self.get_response(url)
         if resp:
             await ctx.send(resp['timeline_txt'])
+            status = resp['status']
+            self.logger.info(f'Try to get:{url}... status={status}')
+        else:
+            self.logger.error('failed to get respons from PriLog')
 
 
 def setup(bot):
