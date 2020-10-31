c27c1f68c80ca752aebb975c949c5e6bc303b766
Teketeke <teketeke.0408@gmail.com>
2020-11-01 00:25:55 +0900

    make it better

--- cogs/prilog.py
+++ cogs/prilog.py
@@ -36,7 +36,34 @@ class PriLog(commands.Cog):
             name = f'総ダメージ: {resp["result"]["total_damage"]}' if \
                 resp["result"]["total_damage"] else '総ダメージ: 不明'
             embed.add_field(name=name,
-                            value=f'```{resp["result"]["timeline_txt"]}```')
+                            value='```'
+                            + resp["result"]["timeline_txt"]
+                            + '```')
+        else:
+            embed = discord.Embed(title='解析失敗',
+                                  discription=f'エラーメッセージ: {resp["msg"]}',
+                                  color=0xed8ab0)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def logb(self, ctx, url: str):
+        """
+        引数にクランバトル動画を渡すとPriLogを利用できます。
+        /logbでは敵UB入りのタイムラインを取得します。
+        例: /logb https://www.youtube.com/watch?v=mvLSw5vCpGU
+        """
+        resp = await self.get_response(url)
+        self.logger.info(f'Try to get:{url}... status={resp["status"]}')
+        if resp["status"] < 310:
+            embed = discord.Embed(title=resp["result"]["title"],
+                                  discription=resp["msg"],
+                                  color=0xe8e8ed)
+            name = f'総ダメージ: {resp["result"]["total_damage"]}' if \
+                resp["result"]["total_damage"] else '総ダメージ: 不明'
+            embed.add_field(name=name,
+                            value='```'
+                            + resp["result"]["timeline_txt_enemy"]
+                            + '```')
         else:
             embed = discord.Embed(title='解析失敗',
                                   discription=f'エラーメッセージ: {resp["msg"]}',
