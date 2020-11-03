ec41677dbe660dc992b7eb32fe08bf3159b648cd
Teketeke <teketeke.0408@gmail.com>
2020-11-04 00:01:13 +0900

    make it better

--- cogs/count_chat.py
+++ cogs/count_chat.py
@@ -107,8 +107,11 @@ class CountChat(commands.Cog):
             for i in self.work_channels.keys():
                 self.work_channels[i][0] += 1
                 channel = self.bot.get_channel(i)
-                msg = await channel.send(f'{self.work_channels[i][0]} 日目開始！\n'
-                                         'このチャットにリアクションを追加して、残凸数を教えてください♪')
+                try:
+                    msg = await channel.send(f'{self.work_channels[i][0]} 日目開始！\n'
+                                             'このチャットにリアクションを追加して、残凸数を教えてください♪')
+                except AttributeError:
+                    return
                 for emoji in EMOJI:
                     await msg.add_reaction(emoji)
                 self.work_channels[i][1] = msg.id
